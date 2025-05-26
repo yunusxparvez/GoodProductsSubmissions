e3e5d917c52a8b36af1819837bd8d74d841daf9a
Welt-Meister <89066061+Welt-Meister@users.noreply.github.com>
2025-05-26 14:23:29 +0530

    fourth commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { createClient } from '@supabase/supabase-js'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
